62b0915b67dbfcf2a3111780ee3f3d6686f5f9b4
Jake Sucoff <jsucoff@gmail.com>
2022-05-23 23:37:54 -0400

    prisma db work

--- pages/api/alphatwt.js
+++ pages/api/alphatwt.js
@@ -1,21 +1,43 @@
-import prisma from "../../lib/prisma";
 import { ethers } from "ethers";
-import { hashCode } from "./../../lib/utils";
 import atob from "atob";
+import NextCors from "nextjs-cors";
+import prisma from "../../lib/prisma";
+import { hashCode } from "./../../lib/utils";
 
 export default async function handle(req, res) {
-  const { alphatwt, userCode } = req.body;
-  const code = JSON.parse(atob(userCode));
-  const creator = ethers.utils.verifyMessage(code.d, code.s);
+  try {
+    console.log("hit the api");
 
-  const result = await prisma.alphaTwt.create({
-    data: {
-      ...alphatwt,
-      creator,
-      published: true,
-    },
-  });
-  res.json({
-    hello: "world",
-  });
+    const { alphatwt, signer } = req.body;
+    // TODO: if no alphatwt or signer throw error
+
+    // const code = JSON.parse(atob(userCode));
+    // const creator = ethers.utils.verifyMessage(code.d, code.s);
+
+    const result = await prisma.alphaTwt.create({
+      data: {
+        ...alphatwt,
+        signer,
+        published: true,
+      },
+    });
+    res.json({
+      result,
+      success: "true",
+    });
+  } catch (e) {
+    // if (e instanceof prisma.PrismaClientKnownRequestError) {
+    //   // The .code property can be accessed in a type-safe manner
+    //   if (e.code === "P2002") {
+    //     console.log(
+    //       "There is a unique constraint violation, a new user cannot be created with this email"
+    //     );
+    //   }
+    // }
+    // throw e;
+    res.json({
+      e,
+      message: e.message,
+    });
+  }
 }

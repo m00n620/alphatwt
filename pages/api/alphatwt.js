eb070df05fab4ab6e892d9d986a6dd89069a7366
Jake Sucoff <jsucoff@gmail.com>
2022-05-23 21:36:13 -0400

    first commit, big diff, pared down p/index, alphatwt.js

--- pages/api/alphatwt.js
+++ pages/api/alphatwt.js
@@ -1,32 +1,21 @@
-import prisma from '../../lib/prisma';
+import prisma from "../../lib/prisma";
 import { ethers } from "ethers";
-import { hashCode } from './../../lib/utils'
+import { hashCode } from "./../../lib/utils";
+import atob from "atob";
 
 export default async function handle(req, res) {
-    const { alphatwt, signature, message } = req.body
-    const payload = JSON.stringify(alphatwt);
-    const creator = await ethers.utils.verifyMessage(message, signature)
-    const [_, hash, time] = message.split('\n')
-    const hashValue = hash.match(/Hash: (.*)/)
-    const timeValue = time.match(/Time: (.*)/)
-    if (hashCode(payload) !== parseInt(hashValue[1], 10)) {
-        return res.json({
-            error: "bad payload"
-        })
-    }
-    if (new Date().getTime() - new Date(timeValue[1]).getTime() - new Date > 1000 * 60) {
-        return res.json({
-            error: "payload too old!"
-        })
-    }
-    const result = await prisma.alphaTwt.create({
-        data: {
-            ...alphatwt,
-            creator,
-            published: true,
-        },
-    });
-    res.json({
-        hello: 'world'
-    });
+  const { alphatwt, userCode } = req.body;
+  const code = JSON.parse(atob(userCode));
+  const creator = ethers.utils.verifyMessage(code.d, code.s);
+
+  const result = await prisma.alphaTwt.create({
+    data: {
+      ...alphatwt,
+      creator,
+      published: true,
+    },
+  });
+  res.json({
+    hello: "world",
+  });
 }

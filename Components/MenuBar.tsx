1e17f395eafb6fede965eee4264f30b01a73197b
m00n620 <gex6611@gmail.com>
2022-07-11 22:22:58 -0400

    fix MenuBar.tsx

--- Components/MenuBar.tsx
+++ Components/MenuBar.tsx
@@ -16,7 +16,12 @@ const MenuBar = ({ extraLink }) => {
     <header className="bg-gray-100 p-2 rounded-lg text-sm flex justify-between">
       <div className="flex">
         <div className="font-semibold">Wallet connected</div>
-        <div className="ml-2">{shortenAddress(signer)}&nsbp;<a onClick={disconnect} href="#">x</a></div>
+        <div className="ml-2">
+          {shortenAddress(signer)}{" "}
+          <a onClick={disconnect} href="#">
+            &times;
+          </a>
+        </div>
       </div>
       <div className="font-semibold">{extraLink}</div>
     </header>
